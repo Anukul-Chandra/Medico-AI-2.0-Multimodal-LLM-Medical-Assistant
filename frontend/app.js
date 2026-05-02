f3186893a8f57f9d05a89df0d05f41009ea71635
Anukul-Chandra <anukulchandra5585@gmail.com>
2026-05-02 09:33:45 +0600

    prepared files for huggingface deployment

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8000";
+const API_URL = ""; // Will use relative URL (same domain)
 
 // DOM Elements
 const loadingOverlay = document.getElementById('loading-overlay');
